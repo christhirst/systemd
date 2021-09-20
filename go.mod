e0987f76a7d5fd3179f3031a649bcad05ac741db
christhirst <chrisgluon@gmail.com>
2021-09-21 00:05:47 +0200

    added oauth

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/gitpod/mycli
 go 1.16
 
 require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/coreos/go-oidc/v3 v3.1.0
 	github.com/coreos/go-systemd v0.0.0-20190321100706-95778dfbb74e
 	github.com/coreos/go-systemd/v22 v22.3.2
 	github.com/dgraph-io/dgo v1.0.0
@@ -11,7 +13,9 @@ require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pquerna/cachecontrol v0.1.0 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
--- handler.go
+++ handler.go
@@ -5,14 +5,34 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/coreos/go-systemd/v22/dbus"
 	"github.com/labstack/echo"
+	"golang.org/x/oauth2"
 )
 
 func (app *application) sendData(c echo.Context) error {
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+	if err != nil {
+		// handle error
+	}
+
+	// Configure an OpenID Connect aware OAuth2 client.
+	oauth2Config := oauth2.Config{
+		ClientID:     "clientID",
+		ClientSecret: "clientSecret",
+		RedirectURL:  "redirectURL",
+
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	fmt.Println(oauth2Config)
 	params := c.QueryParams()
 
-	ctx := context.Background()
 	var stringL []string
 	for _, v := range params {
 		stringL = append(stringL, v[0])
@@ -22,6 +42,10 @@ func (app *application) sendData(c echo.Context) error {
 	con, err := dbus.NewUserConnectionContext(ctx)
 	res, _ := con.ListUnitsByNamesContext(ctx, stringL)
 	con.RestartUnitContext(ctx, "gnome-keyring-ssh.service", "replace", nil)
+	/* 	con.StopUnitContext()
+	   	con.StartUnitContext()
+	   	con.KillUnitContext() */
+
 	fmt.Println(con)
 	if err != nil {
 		fmt.Println(err)
