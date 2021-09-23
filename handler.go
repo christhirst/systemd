8723d3fd60944b1b30cf46fe814971e6c8148325
christhirst <chrisgluon@gmail.com>
2021-09-23 22:47:40 +0200

    channels implemented

--- handler.go
+++ handler.go
@@ -5,34 +5,63 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/coreos/go-systemd/v22/dbus"
 	"github.com/labstack/echo"
-	"golang.org/x/oauth2"
 )
 
-func (app *application) sendData(c echo.Context) error {
+type (
+	action struct {
+		ID      string   `json:"id"`
+		DO      string   `json:"do"`
+		SERVICE []string `json:"service"`
+	}
+)
+
+func (app *application) sendAction(c echo.Context) error {
+
+	u := &action{}
+
+	if err := c.Bind(u); err != nil {
+		return err
+	}
 	ctx := context.Background()
-	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+
+	con, err := dbus.NewUserConnectionContext(ctx)
+	//res, _ := con.ListUnitsByNamesContext(ctx, u.SERVICE)
+
+	ch := make(chan string)
+
+	fmt.Println(u)
+	switch u.DO {
+	case "restart":
+		con.RestartUnitContext(ctx, u.SERVICE[0], "replace", ch)
+		fmt.Println("Today.")
+	case "stop":
+		con.StopUnitContext(ctx, u.SERVICE[0], "replace", ch)
+		fmt.Println("Tomorrow.")
+	case "start":
+		con.StartUnitContext(ctx, u.SERVICE[0], "replace", ch)
+		fmt.Println("In two days.")
+	case "kill":
+		con.KillUnitContext(ctx, u.SERVICE[0], 9)
+	default:
+		close(ch)
+	}
+	x := <-ch
+	fmt.Println("eee")
+	fmt.Println(x)
 	if err != nil {
-		// handle error
+		fmt.Println(err)
 	}
+	//cmd.Execute()
+	defer con.Close()
+	return c.JSON(http.StatusOK, nil)
+}
+func (app *application) status(c echo.Context) error {
 
-	// Configure an OpenID Connect aware OAuth2 client.
-	oauth2Config := oauth2.Config{
-		ClientID:     "clientID",
-		ClientSecret: "clientSecret",
-		RedirectURL:  "redirectURL",
-
-		// Discovery returns the OAuth2 endpoints.
-		Endpoint: provider.Endpoint(),
-
-		// "openid" is a required scope for OpenID Connect flows.
-		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
-	}
-	fmt.Println(oauth2Config)
 	params := c.QueryParams()
 
+	ctx := context.Background()
 	var stringL []string
 	for _, v := range params {
 		stringL = append(stringL, v[0])
@@ -41,11 +70,6 @@ func (app *application) sendData(c echo.Context) error {
 	fmt.Println(stringL)
 	con, err := dbus.NewUserConnectionContext(ctx)
 	res, _ := con.ListUnitsByNamesContext(ctx, stringL)
-	con.RestartUnitContext(ctx, "gnome-keyring-ssh.service", "replace", nil)
-	/* 	con.StopUnitContext()
-	   	con.StartUnitContext()
-	   	con.KillUnitContext() */
-
 	fmt.Println(con)
 	if err != nil {
 		fmt.Println(err)
--- routes.go
+++ routes.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
 func routes(e *echo.Echo, app *application) *echo.Echo {
 
-	e.GET("/user", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users/get")
-	})
-	e.GET("/user", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users/get")
-	})
+	e.POST("/status", app.status)
 
-	e.GET("/test", app.sendData)
+	e.POST("/manager", app.sendAction)
 
 	e.Logger.Printf("dd")
 	e.Start(":8888")
